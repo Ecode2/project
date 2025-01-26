0f635678d94287d8e3caceff2351fa62258db176
Ecode2 <ecode5814@gmail.com>
2025-01-26 11:04:50 +0100

    addded missing libraries to package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+//import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { BottomNav } from '@/components/bottom-nav';
 import { Toaster } from '@/components/ui/toaster';
 import AuthProvider from '@/components/auth/AuthContext';
 
-const inter = Inter({ subsets: ['latin'] });
+//const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'BookVerse - Your Digital Reading Companion',
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <AuthProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={inter.className}>
+        <body> {/* className={inter.className}>*/}j
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
