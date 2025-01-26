4fad3bd5a7021453bed592c90c67a4de195a6014
Ecode2 <ecode5814@gmail.com>
2025-01-26 16:25:49 +0100

    remove some dependencies

--- next.config.js
+++ next.config.js
@@ -9,3 +9,32 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+/* ,
+    "react-day-picker": "^1.0.0",
+    "embla-carousel-react": "^1.0.0",
+    "recharts": "^1.0.0",
+    "cmdk": "^1.0.0",
+    "vaul": "^1.0.0",
+    "input-otp": "^1.0.0",
+    "react-resizable-panels": "^1.0.0",
+    "sonner": "^1.0.0" */
+
+/* 
+    "@radix-ui/react-slot": "^1.0.0",
+    "@radix-ui/react-context-menu": "^1.0.0",
+    "@radix-ui/react-menubar": "^1.0.0",
+    "@radix-ui/react-navigation-menu": "^1.0.0",
+    "@radix-ui/react-separator": "^1.0.0",
+    "@radix-ui/react-accordion": "^1.0.0",
+    "@radix-ui/react-aspect-ratio": "^1.0.0",
+    "@radix-ui/react-avatar": "^1.0.0",
+    "@radix-ui/react-checkbox": "^1.0.0",
+    "@radix-ui/react-collapsible": "^1.0.0",
+    "@radix-ui/react-dropdown-menu": "^1.0.0",
+    "@radix-ui/react-hover-card": "^1.0.0",
+    "@radix-ui/react-popover": "^1.0.0",
+    "@radix-ui/react-radio-group": "^1.0.0",
+    "@radix-ui/react-scroll-area": "^1.0.0",
+    "@radix-ui/react-tooltip": "^1.0.0",
+    "@radix-ui/react-toggle-group": "^1.0.0", */
